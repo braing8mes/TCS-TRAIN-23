5f942811e3dc7ca005c6270d726eb8e1fa8c6920
Brian Liu <brianliu343@gmail.com>
2023-01-16 17:27:58 -0500

    Add overall trends data analysis
    Add data analysis for general sentiments from mid-course survey and on post course survey demographics

---
+++ demographics.py
@@ -0,0 +1,68 @@
+"""
+Pre and post course survey analysis.
+
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('fivethirtyeight')
+pre = pd.read_csv("22-23 Quantum Course Pre-Survey_January 12, 2023_21.28.csv")
+post = pd.read_csv("22-23 Quantum Course Post Semester 1 Survey_January 12, 2023_21.34.csv")
+pre_demo = pre.loc[:,"Q110_1": "Q110_9"]
+# for column in pre_demo:
+    # print(pre_demo[column].value_counts())
+
+pre_race={
+    "Native American or Alaska Native": 29,
+    "Asian": 1633,
+    "White": 847,
+    "Hawaiian or Pacific Islander": 21,
+    "Black or African American": 478,
+    "Hispanic or Latine/o/a/x": 423,
+    "Middle Eastern or North African": 343,
+    "Prefer not to say": 252,
+    "Other": 202,
+}
+post_demo = post.loc[:, "Q124_1": "Q124_9"]
+# for column in post_demo:
+    # print(post_demo[column].sum())
+post_race={
+    "Native American or Alaska Native": 4,
+    "Asian": 69,
+    "White": 40,
+    "Hawaiian or Pacific Islander": 0,
+    "Black or African American": 25,
+    "Hispanic or Latine/o/a/x": 15,
+    "Middle Eastern or North African": 43,
+    "Prefer not to say": 14,
+    "Other": 7,
+}
+demo_rate={}
+for key in pre_race.keys():
+    demo_rate[key] = round(100*post_race[key]/pre_race[key], 1)
+print(demo_rate)
+
+pre_gen = pre.loc[:,"Q109"]
+post_gen = post.loc[:, "Q123"]
+# print(pre_gen.value_counts())
+# print(post_gen.value_counts())
+pre_gender={
+    "Male": 2434,
+    "Female": 1263,
+    "Prefer not to say": 99,
+    "Non-binary": 49,
+    "Other": 18,
+}
+post_gender={
+    "Male": 111,
+    "Female": 72,
+    "Prefer not to say": 11,
+    "Non-binary": 3,
+    "Other": 1,
+}
+gender_rate={}
+for key in pre_gender.keys():
+    gender_rate[key] = round(100*post_gender[key]/pre_gender[key], 1)
+print(gender_rate)
--- general_sentiment.py
+++ general_sentiment.py
@@ -3,3 +3,97 @@ General sentiments on the course comparing pre-course survey to post-course surv
 Mid course survey feelings.
 
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('fivethirtyeight')
+
+# Mid course survey
+
+mid = pd.read_csv("Mid-Course Survey Survey Student Analysis Report (1).csv")
+useful = []
+for each in mid.columns:
+    if each.startswith("1.0") == False:
+        useful.append(each)
+mid_clean = mid[useful]
+
+# interest questions
+num_conv = {
+"Significantly more interested": 1,
+"More interested": 2,
+"Stayed the same": 3,
+"Less interested": 4,
+"Significantly less interested": 5,
+}
+interest = mid_clean[['6572: After one semester of this course, how has your interest in STEM (science, technology, engineering, and math) changed?',
+       '6573: After one semester of this course, how has your interest in quantum computing changed?']]
+stem = mid_clean['6572: After one semester of this course, how has your interest in STEM (science, technology, engineering, and math) changed?'].value_counts(normalize=True)
+quantum = mid_clean['6573: After one semester of this course, how has your interest in quantum computing changed?'].value_counts(normalize=True)
+
+colors = plt.get_cmap('Blues')(np.linspace(0.9, 0.2, len(stem)))
+
+explode = (0, 0, 0, 0, 0)
+f1 = plt.figure(1)
+patches, texts = plt.pie(stem, colors= colors, radius= 40, wedgeprops={"linewidth": 0.3, "edgecolor": "white"}, explode=explode)
+
+plt.title("STEM Interest Change")
+
+plt.axis("equal")
+labels = [f'{l}, {s:.1f}%' for l, s in zip(list(num_conv.keys()), stem*100)]
+plt.legend(patches, labels, loc='best',
+    fontsize=8)
+plt.tight_layout()
+
+f2=plt.figure(2)
+patches2, texts2 = plt.pie(quantum, colors= colors, radius= 40, wedgeprops={"linewidth": 0.3, "edgecolor": "white"}, explode=explode)
+
+plt.title("Quantum Interest Change")
+
+plt.axis("equal")
+labels2 = [f'{l}, {s:.1f}%' for l, s in zip(list(num_conv.keys()), quantum*100)]
+plt.legend(patches2, labels2, loc='best',
+    fontsize=8)
+plt.tight_layout()
+
+diff = mid_clean["6575: How would you rate the level of difficulty of the first semester? "].value_counts(normalize=True)
+labels = list(diff.index)
+f3 = plt.figure(3)
+patches3, texts3 = plt.pie(diff, colors= colors, radius= 40, wedgeprops={"linewidth": 0.3, "edgecolor": "white"})
+
+plt.title("Level of Difficulty")
+
+plt.axis("equal")
+labels3 = [f'{l}, {s:.1f}%' for l, s in zip(labels, diff*100)]
+plt.legend(patches3, labels3, loc='best',
+    fontsize=8)
+plt.tight_layout()
+
+
+overall = mid_clean['6576: On a scale of 1-5, how would you rate the first semester of the course overall?'].value_counts(normalize=True)
+labels = list(overall.index)
+f4 = plt.figure(4)
+patches4, texts4 = plt.pie(overall, colors= colors, radius= 40, wedgeprops={"linewidth": 0.3, "edgecolor": "white"}, explode=explode)
+
+plt.title("Overall Rating")
+
+plt.axis("equal")
+labels4 = [f'{l}, {s:.1f}%' for l, s in zip(labels, overall*100)]
+plt.legend(patches4, labels4, loc='best',
+    fontsize=8)
+plt.tight_layout()
+
+confidence = mid_clean['6574: After semester 1, my confidence in my STEM skills has:'].value_counts(normalize=True)
+f5 = plt.figure(5)
+labels = list(confidence.index)
+patches5, texts5 = plt.pie(confidence, colors= colors, radius= 40, wedgeprops={"linewidth": 0.3, "edgecolor": "white"}, explode=explode)
+
+plt.title("STEM confidence")
+
+plt.axis("equal")
+labels5 = [f'{l}, {s:.1f}%' for l, s in zip(labels, confidence*100)]
+plt.legend(patches5, labels5, loc='best',
+    fontsize=8)
+plt.tight_layout()
+plt.show()
